502c003a54dbeb10aac76f284920221c4849bfb1
heykoushik <heykoushik@gmail.com>
2020-04-17 14:37:03 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT||3000
+app.use(express.static('public'))
+app.get('/', (req, res) => res.sendFile(__dirname + '/public/html/index.html'))
+
+var obj = {}
+obj.name = "Paramkusam Koushik";
+obj.college = "CMR College Of Engineering & Technology";
+obj.rollno = "17H51A04G1";
+
+app.get('/data',(req,res) => res.json(obj))
+app.get('/resume',(req,res) => res.sendFile(__dirname + '/public/html/resume.html'))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
